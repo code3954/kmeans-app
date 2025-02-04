975192aa8ccde9b39a6af8d20d14a64d89469c56
code3954 <namju978@gmail.com>
2025-02-04 12:41:12 +0900

    Update app.py

--- app.py
+++ app.py
@@ -88,8 +88,8 @@ def main() :
         fig1 = plt.figure()
         plt.plot( range(1, max_k+1) ,  wcss )
         plt.title('The Elbow Method')
-        plt.xlabel('# of Clussters')
-        plt.ylabel('WCSS')
+        plt.xlabel('쿨러스터 갯수')
+        plt.ylabel('WCSS 값')
         st.pyplot( fig1 ) 
         
     
